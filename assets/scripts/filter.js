218c9b4d963f6df73a1ae1cb97d8648f08de01ed
rpradosilva <contato@rprado.design>
2021-07-26 20:22:39 -0300

    refactor: update mobile menu

--- assets/scripts/filter.js
+++ assets/scripts/filter.js
@@ -1,5 +1,11 @@
-function teste() {
+function loadFilter() {
   const loadContent = document.querySelectorAll(".aside__menu-list a");
   loadContent[0].click();
 }
-teste();
+loadFilter();
+
+function expandFilter() {
+  const navList = document.querySelector(".aside__menu-list");
+  navList.classList.toggle("open");
+}
+expandFilter();
